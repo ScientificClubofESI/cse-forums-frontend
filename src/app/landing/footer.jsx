69c3d3856239ef8cdc686a52d38e82d8f0248c51
Rami Bendifallah <bendifallahrachid@gmail.com>
2024-12-25 15:11:24 +0100

    second commit changes done...

--- src/app/landing/footer.jsx
+++ src/app/landing/footer.jsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 export default function FooterPage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="flex-grow">
-      </div>
+      <div className="flex-grow"></div>
 
-      <footer  className="py-6 px-3 sm:py-10 sm:px-4 relative bg-slate-300">
+      <footer className="py-6 px-3 sm:py-10 sm:px-4 relative bg-light">
         <div className="max-w-4xl mx-auto text-center">
           <h2 className="text-[#133149] text-3xl sm:text-5xl font-semibold mb-7">
             STILL HAVE <br />
             QUESTIONS ABOUT
           </h2>
-          <div className="flex justify-center items-center gap-8 sm:gap-16 mb-6">
+          <div className="flex justify-center items-center gap-8 sm:gap-16 mb-10">
             <Image
               src="/footer/logo cse.svg"
               alt="CSE"
@@ -28,21 +27,22 @@ export default function FooterPage() {
                 height={140}
                 className="sm:w-[174.8px] sm:h-[188.25px]"
               />
-              <div className="absolute -right-12 sm:-right-16 -top-8 sm:-top-12">
+              <div className="absolute -right-9 sm:-right-20 -top-16 sm:-top-24">
                 <Image
                   src="/footer/Arrow 13.svg"
                   alt="Arrow"
-                  width={70}
-                  height={50}
+                  width={36.73}
+                  height={112.1}
+                  className="sm:w-[73.45px] sm:h-[204.21px]"
                 />
               </div>
             </div>
           </div>
-          <h1 className="text-[#133149] font-sans font-semibold text-2xl sm:text-5xl text-center">
+          <h1 className="text-[#133149] font-sans font-semibold text-2xl sm:text-5xl text-center mb-4 sm:mb-6">
             CONTACT US
           </h1>
 
-          <p className="text-[#262626] mb-4 sm:mb-6 font-serif font-extralight text-sm sm:text-base text-center mt-2">
+          <p className="text-[#262626] font-serif font-extralight text-sm sm:text-base text-center mt-2 mb-6">
             By email{" "}
             <a href="mailto:cse@esi.dz" className="text-blue-600 underline">
               cse@esi.dz
@@ -53,7 +53,7 @@ export default function FooterPage() {
             <a
               href="https://www.facebook.com/club.scientifique.esi/?locale=fr_FR"
               aria-label="Facebook"
-              className="transform hover:scale-110 transition-transform duration-300 ease-in-out "
+              className="transform hover:scale-110 transition-transform duration-300 ease-in-out"
             >
               <Image
                 src="/footer/facebook.svg"
