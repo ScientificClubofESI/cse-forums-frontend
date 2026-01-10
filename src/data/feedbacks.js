cf260f2d437f2bf98493cd6a18a977c9d320a0c8
ikramchouider <li_chouider@esi.dz>
2026-01-10 16:56:44 +0100

    adding images

--- src/data/feedbacks.js
+++ src/data/feedbacks.js
@@ -1,29 +1,24 @@
-import raid from "https://res.cloudinary.com/cse-cse/image/upload/v1744405021/website-member/photo%20profil%20-%20DJEMAI%20MOHAMEDERRAID.jpg";
-import anes from "https://res.cloudinary.com/cse-cse/image/upload/v1755714753/cseclub/kite6rwckugrxfbf8epd.jpg"
-import ayoub from "https://res.cloudinary.com/cse-cse/image/upload/v1755714738/cseclub/ziwezxkb4azha89jf6am.jpg"
-import hiba from "https://res.cloudinary.com/cse-cse/image/upload/v1755714794/cseclub/fgmyufrleeujc2t9eizk.jpg"
 export const feedbacks = [
     {
-      photo:raid,  
+      photo: "https://res.cloudinary.com/cse-cse/image/upload/v1744405021/website-member/photo%20profil%20-%20DJEMAI%20MOHAMEDERRAID.jpg",  
       username: "DJEMAI Mohamed Erraid",
       feedback:
         "A very intuitive platform that truly facilitates interaction between students and developers.",
     },
     {
-      photo:anes, 
+      photo: "https://res.cloudinary.com/cse-cse/image/upload/v1755714753/cseclub/kite6rwckugrxfbf8epd.jpg", 
       username: "Hannachi Anes",
       feedback:
         "CSE Forum helped me quickly find solutions to complex technical problems.",
     },
     {
-      photo:ayoub,   
+      photo: "https://res.cloudinary.com/cse-cse/image/upload/v1755714738/cseclub/ziwezxkb4azha89jf6am.jpg",   
       username: "KIHAL AYOUB",
-      feedback:"An excellent space to learn, share, and grow in the field of computer science."    },
-    {
-      photo:hiba,  
-      username: "Lammari Hiba",
-      feedback:
-"A project that perfectly reflects the CSE Club’s values: sharing and excellence."
+      feedback: "An excellent space to learn, share, and grow in the field of computer science."
     },
-  ];
-  
+    {
+      photo: "https://res.cloudinary.com/cse-cse/image/upload/v1755714794/cseclub/fgmyufrleeujc2t9eizk.jpg",  
+      username: "Lammari Hiba",
+      feedback: "A project that perfectly reflects the CSE Club's values: sharing and excellence."
+    },
+];
